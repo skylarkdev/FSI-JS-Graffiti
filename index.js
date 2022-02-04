3fc328d38ffe796fd687ea7afaff50b11d45b70f
Clayton Burns <claytonb@cooperative-networks.com>
2022-02-03 19:12:09 -0600

    Completed Graffiti Script

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+let header = document.querySelector('#page-header')
+header.style.textAlign = "left"
 
-// Select some elements...
+let dogImages = document.querySelectorAll('.dog-image')
+for(let i = 0; i < dogImages.length; i++){
+    dogImages[i].style.borderRadius = '50px'
+    dogImages[i].style.transform = 'rotate(180deg)'
+}
+
+let dogNames = document.querySelectorAll('.dog-name')
+for(let i = 0; i < dogNames.length; i++){
+    dogNames[i].style.textAlign = 'left'
+}
+
+let footer = document.querySelector('.footer')
+footer.style.color = 'blue'
+
